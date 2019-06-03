29af2c57c6f75f47b627d5fc53082c81c676b6f6
Carpathea <tom@gemstudios.co.uk>
2019-06-03 20:03:46 +0100

    Test throw on 0 input constructed and passed

---
+++ Katas/Katas/RomanNumeralConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        public RomanNumeralConverter() { }
+        public string Convert_to_numeral(int input)
+        {
+            throw new Exception("0 is not known to Roman Numerals");
+        }
+    }
+}
---
+++ Katas/RomanNumeralsTests/RomanNumeralsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using System;
+
+namespace RomanNumeralsTests
+{
+    [TestFixture]
+    public class RomanNumeralsTests
+    {
+        [Test]
+        public void Should_not_recognize_zero()
+        {
+            RomanNumeralConverter numeralConverter = new RomanNumeralConverter();
+            var ex = Assert.Throws<Exception>(() => numeralConverter.Convert_to_numeral(0));
+            Assert.That(ex.Message, Is.EqualTo("0 is not known to Roman Numerals"));
+        }
+
+    }
+}
