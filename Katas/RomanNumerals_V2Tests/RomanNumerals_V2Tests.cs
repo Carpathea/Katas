29a7acdab2752e314a76dc32f89a90fc5e8db4e5
Carpathea <tom@gemstudios.co.uk>
2019-07-15 17:34:10 +0100

    closing off this kata for now to try a new one

--- Katas/RomanNumerals_V2Tests/RomanNumerals_V2Tests.cs
+++ Katas/RomanNumerals_V2Tests/RomanNumerals_V2Tests.cs
@@ -9,6 +9,7 @@ namespace RomanNumerals_V2Tests
     {
         [TestCase(1, "I")]
         [TestCase(2, "II")]
+        [TestCase(3, "III")]
         public void Should_convert_to_numeral(int number, string expected)
         {
             Assert.That(new RomanNumeralConverter().ConvertToRomanNumeral(number), Is.EqualTo(expected));
