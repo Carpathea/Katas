d7f5aea792c2ba2496676c4716147c7633a93256
Carpathea <tom@gemstudios.co.uk>
2019-07-15 18:13:23 +0100

    refactored implimentation

--- Katas/FizzBuzz/FizzBuzz.cs
+++ Katas/FizzBuzz/FizzBuzz.cs
@@ -7,11 +7,7 @@ public class FizzBuzz
 
     public string Fizzify(int input)
     {
-        if (input == 3)
-            return "fizz";
-        else if (input == 6)
-            return "fizz";
-        else if (input == 9)
+        if (input % 3 == 0)
             return "fizz";
         return input.ToString();
     }
--- Katas/FizzBuzzTest/Changelog.md
+++ Katas/FizzBuzzTest/Changelog.md
@@ -11,3 +11,5 @@
 *Update: test case for 3 and minimum passing code.
 *Update: test case for 6 and minimum passing code.
 *Update: test case for 9 and minimum passing code.
+*Note: refactoring now due to rule of 3.
+*Update: refactored implimnentation.
