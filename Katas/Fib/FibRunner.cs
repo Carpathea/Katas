d67aca2b62ea577e27116bd4a0ff6d4806f94950
Carpathea <tom@gemstudios.co.uk>
2019-07-28 18:27:10 +0100

    solution achieved without recursion

--- Katas/Fib/FibRunner.cs
+++ Katas/Fib/FibRunner.cs
@@ -12,5 +12,21 @@ namespace Fib
                 return position;
             return GenerateFibs(position - 1) + GenerateFibs(position - 2);
         }
+        
+        public int GenerateFibsNoRecursion(int position)
+        {
+            if (position < 2)
+                return position;
+            var output = 0;
+            var previous = 1;
+            var secondPrevious = 0;
+            for (int i = 2; i <= position; i++)
+            {
+                output = previous + secondPrevious;
+                secondPrevious = previous;
+                previous = output;
+            }
+            return output;
+        }
     }
 }
--- Katas/FibTest/Class1.cs
+++ Katas/FibTest/Class1.cs
@@ -5,20 +5,8 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
-        [Test]
-        public void Returns_0th_entry()
-        {
-            FibRunner fibRunner = new FibRunner();
-            var position = 0;
-            var result = fibRunner.GenerateFibs(position);
-            Assert.AreEqual(result, 0);
-        }
-
+        [TestCase(0,0)]
         [TestCase(1, 1)]
         [TestCase(2, 1)]
         [TestCase(3, 2)]
@@ -28,7 +16,21 @@ namespace Tests
         {
             FibRunner fibRunner = new FibRunner();
             var result = fibRunner.GenerateFibs(position);
-            Assert.AreEqual(result, expected);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(35, 9227465)]
+        public void Calculates_number_at_pos_no_recursion(int position, int expected)
+        {
+            FibRunner fibRunner = new FibRunner();
+            var result = fibRunner.GenerateFibsNoRecursion(position);         
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }
